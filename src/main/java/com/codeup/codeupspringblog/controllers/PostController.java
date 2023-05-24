ec64e768cce521cf85cd146d1889b52de4aad324
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-05-24 11:31:55 -0500

    completed spring controllers exercise

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -0,0 +1,32 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String posts(){
+        return "post index page.";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String individualPost(@PathVariable int id){
+        return "View an individual post.";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String viewPost(){
+        return "View the form for creating a post.";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String createPost(){
+        return "Create a new post.";
+    }
+}
